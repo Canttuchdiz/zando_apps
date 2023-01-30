cd9f0f5f75028ff6cec0f668b23a7283017d2642
canttuchdiz <hgoldrich@gmail.com>
2023-01-29 20:20:14 -0500

    Important addition, required for users. Will not increase any version num.

--- zando/cogs/help.py
+++ zando/cogs/help.py
@@ -1,4 +1,7 @@
 from zando import *
+from discord import app_commands
+from discord.ext import commands
+import discord
 from zando.utils import Config
 from discord.errors import Forbidden
 from typing import Optional
@@ -40,108 +43,18 @@ class Help(commands.Cog):
     """
 
     def __init__(self, bot):
-        self.bot = bot
+        self.client : commands.Bot = bot
 
-    @commands.hybrid_command(name='info', with_app_command=True)
-    async def info(self, ctx, input : Optional[str] = ''):
-        """Helps showing commands and command descriptions."""
-        input = input.split()
-
-        separator = ', '
-        # !SET THOSE VARIABLES TO MAKE THE COG FUNCTIONAL!
-        prefix = '!'  # ENTER YOUR PREFIX - loaded from config, as string or how ever you want!
-        version = Config.VERSION # enter version of your code
-
-        # setting owner name - if you don't wanna be mentioned remove line 49-60 and adjust help text (line 88)
-        owner =  520741459478052886# ENTER YOU DISCORD-ID
-        owner_name = 'Canttuchdiz#7629' # ENTER YOUR USERNAME#1234
-
-        # checks if cog parameter was given
-        # if not: sending all modules and commands not associated with a cog
-        if not input:
-            # checks if owner is on this server - used to 'tag' owner
-            try:
-                owner = ctx.guild.get_member(owner).mention
-
-            except AttributeError as e:
-                owner = owner
-
-            # starting to build embed
-            emb = discord.Embed(title='Commands and modules', color=discord.Color.blue(),
-                                description=f'Use `{prefix}help <module>` to gain more information about that module '
-                                            f':smiley:\n')
-
-            # iterating trough cogs, gathering descriptions
-            cogs_desc = ''
-            for cog in self.bot.cogs:
-                cogs_desc += f'`{cog}` {self.bot.cogs[cog].__doc__}\n'
-
-            # adding 'list' of cogs to embed
-            emb.add_field(name='Modules', value=cogs_desc, inline=False)
-
-            # integrating trough uncategorized commands
-            commands_desc = ''
-            for command in self.bot.walk_commands():
-                # if cog not in a cog
-                # listing command if cog name is None and command isn't hidden
-                if not command.cog_name and not command.hidden:
-                    commands_desc += f'{command.name} - {command.help}\n'
-
-            # adding those commands to embed
-            if commands_desc:
-                emb.add_field(name='Not belonging to a module', value=commands_desc, inline=False)
-
-            # setting information about author
-            emb.add_field(name="About", value=f"The Bots is developed by Canttuchdiz#7629, based on discord.py.\n\
-                                    This version of it is maintained by {owner}.")
-            emb.set_footer(text=f"Bot is running {version}")
-
-        # block called when one cog-name is given
-        # trying to find matching cog and it's commands
-        elif len(input) == 1:
-
-            # iterating trough cogs
-            for cog in self.bot.cogs:
-                # check if cog is the matching one
-                if cog.lower() == input[0].lower():
-
-                    # making title - getting description from doc-string below class
-                    emb = discord.Embed(title=f'{cog} - Commands', description=self.bot.cogs[cog].__doc__,
-                                        color=discord.Color.green())
-                    cog_commands = self.bot.get_cog(cog).get_commands()
-                    # getting commands from cog
-                    for command in cog_commands:
-                        command_list = []
-                        # if cog is not hidden
-                        if command.aliases:
-                            command_list.append(separator.join(command.aliases))
-                        command_list.append(command.name)
-                        emb.add_field(name=f"`{prefix}{command_list}`", value=command.help, inline=False)
-                    break
-
-            # if input not found
-            # yes, for-loops have an else statement, it's called when no 'break' was issued
-            else:
-                emb = discord.Embed(title="What's that?!",
-                                    description=f"I've never heard from a module called `{input[0]}` before :scream:",
-                                    color=discord.Color.orange())
-
-        # too many cogs requested - only one at a time allowed
-        elif len(input) > 1:
-            emb = discord.Embed(title="That's too much.",
-                                description="Please request only one module at once :sweat_smile:",
-                                color=discord.Color.orange())
-
-        else:
-            emb = discord.Embed(title="It's a magical place.",
-                                description="I don't know how you got here. But I didn't see this coming at all.\n"
-                                            "Would you please be so kind to report that issue to me on github?\n"
-                                            "https://github.com/nonchris/discord-fury/issues\n"
-                                            "Thank you! ~Chris",
-                                color=discord.Color.red())
-
-        # sending reply embed using our own function defined above
-        await send_embed(ctx, emb)
+    @app_commands.command(name='help', description='Lists all available commands')
+    async def help(self, interaction : discord.Interaction):
+        emb = discord.Embed(title="Help", description='Zando Apps is a feature rich and customizable questionnaire and application bot to help enhance your community!',
+                            color=discord.Color.blue())
+        slash_cmds = [f"``/{command.name}``" for command in self.client.tree.get_commands()]
+        cmds = '**|**'.join(slash_cmds)
+        emb.add_field(name="Commands", value=cmds)
+        emb.set_author(name=self.client.user.name, icon_url=self.client.user.avatar)
+        emb.set_footer(text=f"ID : {interaction.user.id}", icon_url=interaction.user.avatar)
+        await interaction.response.send_message(embed=emb)
 
 
 async def setup(bot):
